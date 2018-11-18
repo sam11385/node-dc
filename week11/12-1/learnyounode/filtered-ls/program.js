63f2c4839e066b4d79ed399a6484c8f87fc055e7
sam11385 <samluedke@gmail.com>
2018-11-18 13:31:55 -0500

    run program to get extension types

--- week11/12-1/learnyounode/filtered-ls/program.js
+++ week11/12-1/learnyounode/filtered-ls/program.js
@@ -1,1 +1,16 @@
 const fs = require('fs');
+const path = require('path');
+
+const dir = process.argv[2];
+const ext = '.' + process.argv[3];
+
+fs.readdir(dir, function(err, files) {
+  if (err) return console.error(err);
+  files.forEach(function(file) {
+    if (path.extname === ext) {
+      console.log(file);
+    }
+  });
+});
+
+// console.log(path.dirname('~/digital-crafts/node-dc/'));
