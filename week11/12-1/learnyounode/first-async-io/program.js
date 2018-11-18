3a09c1811f6f1b4c7e16c3e290c37cd33a71fa9b
sambsbdev <sam.luedke@bluesombrero.com>
2018-11-18 13:03:22 -0500

    Set program to work

--- week11/12-1/learnyounode/first-async-io/program.js
+++ week11/12-1/learnyounode/first-async-io/program.js
@@ -1,1 +1,10 @@
 const fs = require('fs');
+const file = process.argv[1];
+
+fs.readFile(file, function(err, contents) {
+  if (err) {
+    console.log(err);
+  }
+  const lines = contents.toString().split('\n').length - 1;
+  console.log(lines);
+});
