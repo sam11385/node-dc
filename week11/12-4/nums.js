952b1dbdb93b8fa9c7302d8f0da63344071036b7
sam11385 <samluedke@gmail.com>
2018-12-02 20:04:33 -0500

    Run lodash exercises.

---
+++ week11/12-4/nums.js
@@ -0,0 +1,7 @@
+const sum = require('lodash/sum.js');
+const max = require('lodash/max.js');
+const mean = require('lodash/mean.js');
+
+let nums = [2, 4, 27, 99];
+
+console.log(sum(nums), max(nums), mean(nums));
